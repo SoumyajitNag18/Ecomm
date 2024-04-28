6e312b663ecf158cc0953e72df4c905b163090d4
SoumyajitNag18 <soumyajitnag18@gmail.com>
2024-04-28 14:53:07 +0530

    Your message

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,9 @@ const multer = require("multer");
 // database setup
 const mongoose = require("mongoose");
 
-
 const path = require ("path");
 
-
 const dbConnect = mongoose.connect('mongodb+srv://admin:admin@cluster0.jsucpf7.mongodb.net/');
-
 
 // make application to serve backend setup
 app.use(express.json());
@@ -33,3 +30,23 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
 })
+
+//Image Storage Engine
+
+const storage= multer.diskStorage({
+    destination:"./upload/images",
+    filename:(req, file, cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage: storage});
+ 
+//Creating upload end points for images
+app.use('/images', express.static('upload/images'));
+app.post('/upload', upload.single('product'),(req,res)=> {
+    res.json({
+        success:1,
+        image_url:`http://localhost:${PORT}/images/${req.file.filename}`
+    });
+});
