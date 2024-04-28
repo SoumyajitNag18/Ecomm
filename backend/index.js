dbc99413a9e4927a7ce339cf0b4f6eea5be0b941
SoumyajitNag18 <soumyajitnag18@gmail.com>
2024-04-28 15:04:07 +0530

    Created Schema for DB

--- backend/index.js
+++ backend/index.js
@@ -50,3 +50,39 @@ app.post('/upload', upload.single('product'),(req,res)=> {
         image_url:`http://localhost:${PORT}/images/${req.file.filename}`
     });
 });
+
+//Creating Schema for MongoDB
+const Product= mongoose.model("Product", {
+    id:{
+        type: Number,
+        required: true,
+    }, 
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    new_price:{
+        type: Number,
+        required: true,
+    },
+    old_price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    },
+    available:{
+        type: Boolean,
+        default: true,
+    },
+});
