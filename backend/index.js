21c42c70ec93a043042b276cf1f72b783904ab7b
SoumyajitNag18 <soumyajitnag18@gmail.com>
2024-04-29 23:12:31 +0530

    Fix: Related to Product ID generation

--- backend/index.js
+++ backend/index.js
@@ -93,8 +93,18 @@ const Product= mongoose.model("Product", {
 });
 
 app.post('/addproduct', async(req, res)=>{
+    let products=await Product.find({});
+    let id;
+    if (products.length > 0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;
+    }
     const product = new Product({
-        id: req.body.id,
+        id: id,
         name: req.body.name,
         image: req.body.image,
         category: req.body.category,
