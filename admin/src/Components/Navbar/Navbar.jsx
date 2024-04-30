f95c84d3ace84ba9711624d74ed7727ca7c05b6e
SoumyajitNag18 <soumyajitnag18@gmail.com>
2024-04-30 19:22:00 +0530

    Added: Admin folder system with some packages

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src={navlogo} alt="Navbar Logo" className="nav-logo" />
+        <img src={navProfile} alt="Profile logo" className="nav-profile" />
+    </div>
+  )
+}
+
+export default Navbar
