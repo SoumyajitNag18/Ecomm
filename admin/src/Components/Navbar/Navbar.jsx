bec2439e2471d6504ac3b7b6b4b1b38293dfdc6d
SoumyajitNag18 <soumyajitnag18@gmail.com>
2024-05-02 01:24:58 +0530

    Added: Navbar and Sidebar with Responsive Design

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
         <img src={navlogo} alt="Navbar Logo" className="nav-logo" />
         <img src={navProfile} alt="Profile logo" className="nav-profile" />
     </div>
