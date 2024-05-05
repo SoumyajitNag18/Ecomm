497a4b7eda44ffa35da7a981cbfa37041b639cd7
SoumyajitNag18 <soumyajitnag18@gmail.com>
2024-05-05 16:04:09 +0530

    Added: Login and Log out Functionality

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -29,7 +29,12 @@ const Navbar = () => {
             <li onClick={()=>{setMenu('kids')}}><Link style = {{textDecoration:'none'}} to='/kids'>Kids</Link>{menu==='kids'?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
-            <Link to='/login'><button>Login</button></Link>
+            {localStorage.getItem('auth-token')
+            ?<button onClick={()=> {
+                localStorage.remove('auth-key');
+                window.location.replace('/');
+            }}>Log out</button>
+            :<Link to='/login'><button>Login</button></Link> }
             <Link to='/cart'><img src={cart_icon} alt=""/></Link>
             <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
